348ac856bdd40c118224fd49db58abc4bc8a2cd3
Hritik <ritikmily143@gmail.com>
2022-09-18 19:56:58 +0530

    how to make indian flag in python progrmming

---
+++ flag_india.py
@@ -0,0 +1,103 @@
+import turtle
+turtle.speed(0)
+turtle.fillcolor("#FF7722")
+turtle.begin_fill()
+turtle.forward(210)
+turtle.left(90)
+turtle.forward(70)
+turtle.left(90)
+turtle.forward(210)
+turtle.left(90)
+turtle.forward(70)
+turtle.left(90)
+turtle.end_fill()
+turtle.right(90)
+turtle.forward(70)
+turtle.left(90)
+turtle.forward(210)
+turtle.left(90)
+turtle.forward(70)
+turtle.left(180)
+turtle.forward(70)
+turtle.fillcolor("green")
+turtle.begin_fill()
+turtle.forward(70)
+turtle.right(90)
+turtle.forward(210)
+turtle.right(90)
+turtle.forward(70)
+turtle.end_fill()
+
+turtle.right(90)
+turtle.forward(210)
+turtle.left(180)
+turtle.forward(70)
+turtle.right(90)
+
+turtle.up()
+turtle.forward(35)
+turtle.down()
+turtle.pencolor("blue")
+turtle.width(2)
+turtle.circle(35)
+turtle.left(90)
+turtle.width(1)
+# turtle.forward(70)
+# turtle.left(180)
+turtle.forward(35)
+# turtle.left(90)
+# turtle.forward(35)
+
+# turtle.left(180)
+# turtle.forward(70)
+# turtle.left(180)
+# turtle.forward(35)
+
+# turtle.left(9.1584)
+# turtle.forward(35)
+# turtle.left(180)
+# turtle.forward(35)
+
+
+for x in range(20):
+    
+    turtle.left(9.158333)
+    turtle.forward(35)
+    turtle.left(180)
+    turtle.forward(70)
+    turtle.left(180)
+    turtle.forward(35)
+# turtle.left(9.1583333)
+turtle.up()
+turtle.left(180)
+turtle.forward(105)
+turtle.down()
+
+turtle.pencolor("black")
+turtle.left(86.75)
+turtle.forward(400)
+
+turtle.fillcolor("brown")
+turtle.begin_fill()
+turtle.right(90)
+turtle.forward(10)
+turtle.right(90)
+turtle.forward(400+125)
+turtle.right(90)
+turtle.forward(10)
+turtle.right(90)
+turtle.forward(20)
+turtle.end_fill()
+
+
+
+
+
+
+
+
+turtle.done()
+
+
+# turtle.fillcolor("WHIT")
+# turtle.begin_fill()
